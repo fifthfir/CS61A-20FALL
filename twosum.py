ca4a5564c0918de0e40c2eac878b5c1ee2a5c91e
fifthfir <fifthfir2017@gmail.com>
2023-09-10 21:21:27 -0700

    Delete

--- twosum.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums, target):
-    for i in range(len(nums)):
-        l = i + 1
-        for j in range(l, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i,j]
-    return []
-    
-twoSum([2,7,11,15], 9)
