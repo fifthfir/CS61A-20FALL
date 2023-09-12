9b52cb82dce7774e3ae0dc5814f90b833b093b8c
fifthfir <fifthfir2017@gmail.com>
2023-09-11 18:00:40 -0700

    Tree is so hard

--- discussion/disc05.py
+++ discussion/disc05.py
@@ -94,3 +94,58 @@ def height(t):
     if is_leaf(t):
         return 0
     return 1 + max([height(branch) for branch in branches(t)])
+
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    if is_leaf(t):
+        return t[0]
+    return t[0] + max(max_path_sum(branch) for branch in branches(t))
+
+def square_tree(t):
+    """Return a tree with the square of every element in t
+    >>> numbers = tree(1, [tree(2, [tree(3), tree(4)]), tree(5, [tree(6, [tree(7)]), tree(8)])])
+    >>> print_tree(square_tree(numbers))
+    1
+      4
+        9
+        16
+      25
+        36
+          49
+        64
+    """
+    return tree(label(t) ** 2, [square_tree(branch) for branch in branches(t)])
+
+def find_path(tree, x):
+    """
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10) # returns None
+    """
+    if x == label(tree):
+        return [x]
+    for branch in branches(tree):
+        path = find_path(branch, x)
+        if path:
+            return [label(tree)] + path
+
+def prune_binary(t, nums):
+    if is_leaf(t):
+        if label(t) in nums:
+            return t
+        return None
+    else:
+        next_valid_nums = [i[1:] for i in nums if i[0] == label(t)]
+        new_branches = []
+        for b in branches(t):
+            pruned_branch = prune_binary(b, next_valid_nums)
+            if pruned_branch is not None:
+                new_branches = new_branches + [pruned_branch]
+        if not new_branches:
+            return None
+        return tree(label(t), new_branches)
