4b5a46180f3d8b730e636cf3d89bbcb05e16fe2e
fifthfir <fifthfir2017@gmail.com>
2023-10-05 18:03:37 -0700

    lab10

---
+++ lecture/link.py
@@ -0,0 +1,110 @@
+def range_link(start, end):
+    """
+    >>> range_link(3, 6)
+    Link(3, Link(4, Link(5)))
+    """
+    if start >= end:
+        return Link.empty
+    else:
+        return Link(start, range_link(start + 1, end))
+
+
+def map_link(f, s):
+    """
+    >>> map_link(square, range_link(3, 6))
+    Link(9, Link(16, Link(25)))
+    """
+    if s is Link.empty:
+        return s
+    else:
+        return Link(f(s.first), map_link(f, s.rest))
+
+
+def filter_link(f, s):
+    """
+    >>> filter_link(odd, range_link(3, 6))
+    Link(3, Link(5))
+    """
+    if s is Link.empty:
+        return s
+    else:
+        filtered_rest = filter_link(f, s.rest)
+        if f(s.first):
+            return Link(s.first, filtered_rest)
+        else:
+            return filtered_rest
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s
+    Link(3, Link(4, Link(5)))
+    >>> print(s)
+    <3 4 5>
+    >>> s.first
+    3
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.first
+    4
+    >>> s.rest.first = 7
+    >>> s
+    Link(3, Link(7, Link(5)))
+    >>> s.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s
+    Link(6, Link(7))
+    >>> print(s)
+    <6 7>
+    >>> print(s.rest)
+    <7>
+    >>> t = Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> t
+    Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> print(t)
+    <1 <2 3> 4>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+square, odd = lambda x: x * x, lambda x: x % 2 == 1
+list(map(square, filter(odd, range(1, 6))))  # [1, 9, 25]
+
+
+class Tree:
+    def __init__(self, label, branches=[]):
+        self.label = label
+        for branch in branches:
+            assert isinstance(branch, Tree)
+        self.branches = list(branches)
+
+
+def fib_tree(n):
+    if n == 0 or n == 1:
+        return Tree(n)
+    else:
+        left = fib_tree(n - 2)
+        right = fib_tree(n - 1)
+        fib_n = left.label + right.label
+        return Tree(fib_n, [left, right])
