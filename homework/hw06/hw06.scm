206196636f84f164f75a019d3f9c2489e08356ff
fifthfir <fifthfir2017@gmail.com>
2023-10-05 19:09:55 -0700

    hw06 easy

--- homework/hw06/hw06.scm
+++ homework/hw06/hw06.scm
@@ -1,23 +1,25 @@
 (define (cddr s)
-  (cdr (cdr s)))
+  (cdr (cdr s))
+)
 
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
-
 
 (define (sign num)
-  'YOUR-CODE-HERE
+  (cond ((> 0 num) -1) ((= 0 num) 0) (else 1))
 )
-
 
 (define (square x) (* x x))
 
 (define (pow x y)
-  'YOUR-CODE-HERE
+  (cond ((= 1 y) x) 
+        ((even? y) (pow (square x) (/ y 2)))
+        (else (* x (pow (square x) (/ (- y 1) 2))))
+  )
 )
 
